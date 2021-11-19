0526600d488da3e9f8342587ed46a1da7b587967
sophia-jihye <jihyeparkk@naver.com>
2021-11-19 22:59:15 +0900

    pt_mode

--- main_finetune_and_test_for_upper_limit_using_post-trained.py
+++ main_finetune_and_test_for_upper_limit_using_post-trained.py
@@ -12,8 +12,7 @@ post_trained_dir_format = '/media/dmlab/My Passport/DATA/cross-domain/post-train
 train_filepaths = glob('/media/dmlab/My Passport/DATA/cross-domain/kfold_train&val/*_train.json')
 val_filepath_format = '/media/dmlab/My Passport/DATA/cross-domain/kfold_train&val/{}_k={}_val.json'
 test_filepath_format = '/media/dmlab/My Passport/DATA/cross-domain/kfold_train&val/{}_k={}_test.json'
-parent_save_dir = '/media/dmlab/My Passport/DATA/cross-domain/finetune_upper_limit_using_post-trained'
-if not os.path.exists(parent_save_dir): os.makedirs(parent_save_dir)
+parent_save_dir_format = '/media/dmlab/My Passport/DATA/cross-domain/finetune_upper_limit_using_post-trained_{}'
 
 def main(model_name_or_dir, num_classes, train_texts, train_labels, val_texts, val_labels, save_dir, device, df):
     tokenizer, model = load_tokenizer_and_model(model_name_or_dir, num_classes=num_classes, mode='classification')
@@ -48,28 +47,29 @@ if __name__=='__main__':
     num_classes = 2
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    for train_filepath in train_filepaths:
-        for mode in ['unk', 'raw']:
-            domain = os.path.basename(train_filepath).split('_')[0]
-            model_name_or_dir = post_trained_dir_format.format(domain, mode)
-            kfold_idx = os.path.basename(train_filepath).split('_')[1].replace('k=', '')
-            save_dir = os.path.join(parent_save_dir, '{}_{}_k={}'.format(domain, mode, kfold_idx))
-            if not os.path.exists(save_dir): os.makedirs(save_dir)
-            val_filepath = val_filepath_format.format(domain, kfold_idx)
-            train_df = pd.read_json(train_filepath)[['text', 'masked_text', 'label']]
-            train_df['label'] = train_df['label'].apply(lambda x: relabel_dict[x])
-            val_df = pd.read_json(val_filepath)[['text', 'masked_text', 'label']]
-            val_df['label'] = val_df['label'].apply(lambda x: relabel_dict[x])
+    for pt_mode in ['unk', 'raw']:
+        for train_filepath in train_filepaths:
+            for mode in ['unk', 'raw']:
+                domain = os.path.basename(train_filepath).split('_')[0]
+                model_name_or_dir = post_trained_dir_format.format(domain, pt_mode)
+                kfold_idx = os.path.basename(train_filepath).split('_')[1].replace('k=', '')
+                save_dir = os.path.join(parent_save_dir_format.format(pt_mode), '{}_{}_k={}'.format(domain, mode, kfold_idx))
+                if not os.path.exists(save_dir): os.makedirs(save_dir)
+                val_filepath = val_filepath_format.format(domain, kfold_idx)
+                train_df = pd.read_json(train_filepath)[['text', 'masked_text', 'label']]
+                train_df['label'] = train_df['label'].apply(lambda x: relabel_dict[x])
+                val_df = pd.read_json(val_filepath)[['text', 'masked_text', 'label']]
+                val_df['label'] = val_df['label'].apply(lambda x: relabel_dict[x])
 
-            if mode == 'unk':
-                train_texts, val_texts = train_df['masked_text'].values, val_df['masked_text'].values
-            elif mode == 'raw':
-                train_texts, val_texts = train_df['text'].values, val_df['text'].values
+                if mode == 'unk':
+                    train_texts, val_texts = train_df['masked_text'].values, val_df['masked_text'].values
+                elif mode == 'raw':
+                    train_texts, val_texts = train_df['text'].values, val_df['text'].values
 
-            train_labels, val_labels = train_df['label'].values, val_df['label'].values
-            
-            test_filepath = test_filepath_format.format(domain, kfold_idx)
-            df = pd.read_json(test_filepath)[['text', 'label']] # Raw texts
-            df['label'] = df['label'].apply(lambda x: relabel_dict[x])
-            df.columns = ['text', 'true_label']
-            main(model_name_or_dir, num_classes, train_texts, train_labels, val_texts, val_labels, save_dir, device, df)
+                train_labels, val_labels = train_df['label'].values, val_df['label'].values
+
+                test_filepath = test_filepath_format.format(domain, kfold_idx)
+                df = pd.read_json(test_filepath)[['text', 'label']] # Raw texts
+                df['label'] = df['label'].apply(lambda x: relabel_dict[x])
+                df.columns = ['text', 'true_label']
+                main(model_name_or_dir, num_classes, train_texts, train_labels, val_texts, val_labels, save_dir, device, df)
