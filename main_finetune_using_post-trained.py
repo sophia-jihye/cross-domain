2511ec4cec13a71998c6712cf5b0757424f4a308
sophia-jihye <jihyeparkk@naver.com>
2021-11-22 18:13:47 +0900

    post-train domain-specific keywords UNK

--- main_finetune_using_post-trained.py
+++ main_finetune_using_post-trained.py
@@ -6,10 +6,10 @@ from transformers_helper import load_tokenizer_and_model
 from CustomDataset import CustomDataset
 import finetuning_classification
 
-post_trained_dirs = [d for d in glob('/media/dmlab/My Passport/DATA/cross-domain/post-train/*&*_raw') if os.path.isdir(d)]
+post_trained_dirs = [d for d in glob('/media/dmlab/My Passport/DATA/cross-domain/post-train/*&*_unk-specific') if os.path.isdir(d)]
 train_filepaths = glob('/media/dmlab/My Passport/DATA/cross-domain/train&val/*_train.json')
 val_filepath_format = '/media/dmlab/My Passport/DATA/cross-domain/train&val/{}_val.json'
-parent_save_dir = '/media/dmlab/My Passport/DATA/cross-domain/finetune_using_post-trained_raw'
+parent_save_dir = '/media/dmlab/My Passport/DATA/cross-domain/finetune_using_post-trained_unk-specific'
 if not os.path.exists(parent_save_dir): os.makedirs(parent_save_dir)
 
 def main(model_name_or_dir, num_classes, train_texts, train_labels, val_texts, val_labels, save_dir):
@@ -25,23 +25,23 @@ def main(model_name_or_dir, num_classes, train_texts, train_labels, val_texts, v
 if __name__=='__main__':
     relabel_dict = {'negative':0, 'positive':1}
     num_classes = 2
+    mode = 'raw'
     
     for train_filepath in train_filepaths:
-        for mode in ['unk', 'raw']:
-            domain = os.path.basename(train_filepath).split('_')[0]
-            model_name_or_dirs = [d for d in post_trained_dirs if domain in d]
-            for model_name_or_dir in model_name_or_dirs:
-                save_dir = os.path.join(parent_save_dir, 'source={}_{}_post={}'.format(domain, mode, os.path.basename(model_name_or_dir)))
-                val_filepath = val_filepath_format.format(domain)
-                train_df = pd.read_json(train_filepath)[['text', 'masked_text', 'label']]
-                train_df['label'] = train_df['label'].apply(lambda x: relabel_dict[x])
-                val_df = pd.read_json(val_filepath)[['text', 'masked_text', 'label']]
-                val_df['label'] = val_df['label'].apply(lambda x: relabel_dict[x])
+        domain = os.path.basename(train_filepath).split('_')[0]
+        model_name_or_dirs = [d for d in post_trained_dirs if domain in d]
+        for model_name_or_dir in model_name_or_dirs:
+            save_dir = os.path.join(parent_save_dir, 'source={}_{}_post={}'.format(domain, mode, os.path.basename(model_name_or_dir)))
+            val_filepath = val_filepath_format.format(domain)
+            train_df = pd.read_json(train_filepath)[['text', 'masked_text', 'label']]
+            train_df['label'] = train_df['label'].apply(lambda x: relabel_dict[x])
+            val_df = pd.read_json(val_filepath)[['text', 'masked_text', 'label']]
+            val_df['label'] = val_df['label'].apply(lambda x: relabel_dict[x])
 
-                if mode == 'unk':
-                    train_texts, val_texts = train_df['masked_text'].values, val_df['masked_text'].values
-                elif mode == 'raw':
-                    train_texts, val_texts = train_df['text'].values, val_df['text'].values
+            if mode == 'unk':
+                train_texts, val_texts = train_df['masked_text'].values, val_df['masked_text'].values
+            elif mode == 'raw':
+                train_texts, val_texts = train_df['text'].values, val_df['text'].values
 
-                train_labels, val_labels = train_df['label'].values, val_df['label'].values
-                main(model_name_or_dir, num_classes, train_texts, train_labels, val_texts, val_labels, save_dir)
+            train_labels, val_labels = train_df['label'].values, val_df['label'].values
+            main(model_name_or_dir, num_classes, train_texts, train_labels, val_texts, val_labels, save_dir)
--- main_test_using_post-trained.py
+++ main_test_using_post-trained.py
@@ -9,8 +9,8 @@ from CustomDataset import encode_for_inference
 import finetuning_classification, reports
 
 test_filepath = '/media/dmlab/My Passport/DATA/cross-domain/MDSD_masked.json'
-finetune_unk_dirs = glob('/media/dmlab/My Passport/DATA/cross-domain/finetune_using_post-trained_raw/source=*_unk_post=*')
-parent_save_dir = '/media/dmlab/My Passport/DATA/cross-domain/test_using_post-trained_raw'
+finetune_raw_dirs = glob('/media/dmlab/My Passport/DATA/cross-domain/finetune_using_post-trained_unk-specific/source=*_raw_post=*')
+parent_save_dir = '/media/dmlab/My Passport/DATA/cross-domain/test_using_post-trained_unk-specific'
 
 def main(device, model_name_or_dir, df, save_dir):
     tokenizer, model = load_tokenizer_and_model(model_name_or_dir, mode='classification')
@@ -40,19 +40,17 @@ if __name__ == '__main__':
     all_domains = test_all_df['domain'].unique()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    for finetune_unk_dir in finetune_unk_dirs:
-        domain = os.path.basename(finetune_unk_dir).split('_')[0].replace('source=','')
-        test_domain = os.path.basename(finetune_unk_dir).split('_')[2].replace('post=','').replace(domain, '').replace('&', '')
-        finetune_raw_dir = finetune_unk_dir.replace('unk', 'raw')
-        
+    for finetune_raw_dir in finetune_raw_dirs:
+        domain = os.path.basename(finetune_raw_dir).split('_')[0].replace('source=','')
+        test_domain = os.path.basename(finetune_raw_dir).split('_')[2].replace('post=','').replace(domain, '').replace('&', '')
         test_df = copy.copy(test_all_df[test_all_df['domain']==test_domain][['masked_text', 'label']])
         test_df.columns = ['text', 'true_label']
-
-        save_dir = os.path.join(parent_save_dir, 'target={}_source={}_{}'.format(test_domain, domain, 'unk'))
-        if not os.path.exists(save_dir): os.makedirs(save_dir)
-        main(device, finetune_unk_dir, test_df, save_dir)
 
         save_dir = os.path.join(parent_save_dir, 'target={}_source={}_{}'.format(test_domain, domain, 'raw'))
         if not os.path.exists(save_dir): os.makedirs(save_dir)
         main(device, finetune_raw_dir, test_df, save_dir)
         
+#         finetune_unk_dir = finetune_raw_dir.replace('raw', 'unk')
+#         save_dir = os.path.join(parent_save_dir, 'target={}_source={}_{}'.format(test_domain, domain, 'unk'))
+#         if not os.path.exists(save_dir): os.makedirs(save_dir)
+#         main(device, finetune_unk_dir, test_df, save_dir)
