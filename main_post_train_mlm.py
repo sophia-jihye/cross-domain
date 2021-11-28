bd39572b940fdb4c877c85cbb7794266f0b84896
sophia-jihye <jihyeparkk@naver.com>
2021-11-28 20:37:15 +0900

    post-train with mlm

---
+++ main_post_train_mlm.py
@@ -0,0 +1,25 @@
+from glob import glob
+import os
+import pandas as pd
+
+from LazyLineByLineTextDataset import LazyLineByLineTextDataset
+from transformers_helper import load_tokenizer_and_model
+import post_training_mlm
+
+post_filepaths = glob('/data/jihye_data/cross-domain/data/MDSD_*&*_MLM_for_post.txt')
+post_parent_save_dir = '/data/jihye_data/cross-domain/post-train'
+post_trained_dirs = [d for d in glob(os.path.join(post_parent_save_dir, '*&*_*')) if os.path.isdir(d)]
+
+def start_post_train(model_name_or_dir, post_filepath, save_dir):
+    tokenizer, model = load_tokenizer_and_model(model_name_or_dir, mode='masking')
+    dataset = LazyLineByLineTextDataset(tokenizer=tokenizer, file_path=post_filepath)
+    post_training_mlm.train(tokenizer, model, dataset, save_dir)
+
+if __name__ == '__main__':
+    
+    model_name_or_dir = 'bert-base-uncased'
+    for post_filepath in post_filepaths:
+        _, domain, mode, _, _ = os.path.basename(post_filepath).split('_')
+        save_dir = os.path.join(post_parent_save_dir, '{}_{}'.format(domain, mode))
+        if not os.path.exists(save_dir): os.makedirs(save_dir)
+        start_post_train(model_name_or_dir, post_filepath, save_dir)
---
+++ prev_LR/post_training_mlm.py
@@ -0,0 +1,31 @@
+from transformers import TrainingArguments, Trainer
+from transformers import DataCollatorForLanguageModeling
+
+def train(tokenizer, model, dataset, save_dir, mlm_prob=0.15):
+    model.train()
+    
+    training_args = TrainingArguments(
+        output_dir = save_dir,
+        num_train_epochs = 5,
+        per_device_train_batch_size = 4,
+        save_steps = 10000,
+        save_total_limit = 1,
+    )
+    
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer = tokenizer, mlm = True, mlm_probability = mlm_prob
+    )
+
+    trainer = Trainer(
+        model = model,
+        args = training_args,
+        data_collator = data_collator,
+        train_dataset = dataset,
+    )
+    
+    print('[Post-training (MLM)] Training..')
+    trainer.train()
+    
+    tokenizer.save_pretrained(save_dir)
+    trainer.save_model(save_dir)
+    print('[Post-training (MLM)] Saved trained model at {}'.format(save_dir))
