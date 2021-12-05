1748885f5579a90e9f4aedebaca9c40bb2d4a814
sophia-jihye <jihyeparkk@naver.com>
2021-12-05 14:12:58 +0900

    ULSimST

--- main_similar_sentence_sampler.py
+++ main_similar_sentence_sampler.py
@@ -21,14 +21,14 @@ def do_experiment(unlabeled_df, source_domain, feature_extractor, domain_save_di
     records = []
     for text in tqdm(unlabeled_df[unlabeled_df['domain']==source_domain]['text'].values):
         vec = feature_extractor.get_cls_embedding(text)
-        records.append((text, vec))
-    source_df = pd.DataFrame(records, columns=['text', 'dom-cls'])
-    filepath = os.path.join(domain_save_dir, '{}_{}_{}.csv'.format('source', 'unlabeled', source_domain))
+        records.append(('unlabeled', text, vec))
+    source_df = pd.DataFrame(records, columns=['from', 'text', 'dom-cls'])
+    filepath = os.path.join(domain_save_dir, '{}_{}.csv'.format('source', source_domain))
     source_df.to_csv(filepath, index=False)
     print('Created {}'.format(filepath))
 
-    # Target texts from labeled data
     records = []
+    # Target texts from labeled data
     for text in tqdm(labeled_df[labeled_df['domain']==target_domain]['text'].values):
         vec = feature_extractor.get_cls_embedding(text)
 
@@ -37,10 +37,22 @@ def do_experiment(unlabeled_df, source_domain, feature_extractor, domain_save_di
         most_similar_text, most_similar_score = most_similar_row['text'], most_similar_row['cos']
         source_df.drop(columns=['cos'], inplace=True)
 
-        records.append((text, vec, most_similar_text, most_similar_score))
+        records.append(('labeled', text, vec, most_similar_text, most_similar_score))
 
-    target_df = pd.DataFrame(records, columns=['text', 'dom-cls', 'most-similar_text', 'most-similar_score'])
-    filepath = os.path.join(domain_save_dir, '{}_{}_{}.csv'.format('target', 'labeled', target_domain))
+    # Target texts from unlabeled data
+    for text in tqdm(unlabeled_df[unlabeled_df['domain']==target_domain]['text'].values):
+        vec = feature_extractor.get_cls_embedding(text)
+
+        source_df['cos'] = source_df['dom-cls'].apply(lambda x: cos_sim(vec, x))
+        most_similar_row = source_df.sort_values(by=['cos'], ascending=False, axis=0).iloc[0]
+        most_similar_text, most_similar_score = most_similar_row['text'], most_similar_row['cos']
+        source_df.drop(columns=['cos'], inplace=True)
+
+        records.append(('unlabeled', text, vec, most_similar_text, most_similar_score))
+
+        
+    target_df = pd.DataFrame(records, columns=['from', 'text', 'dom-cls', 'most-similar_text', 'most-similar_score'])
+    filepath = os.path.join(domain_save_dir, '{}_{}.csv'.format('target', target_domain))
     target_df.to_csv(filepath, index=False)
     print('Created {}'.format(filepath))
     
